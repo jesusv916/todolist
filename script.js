c4ea52860ad79bc30844e229bac4a225b4723551
jesusv916 <jevamon916@hotmail.com>
2023-03-30 17:23:39 -0500

    adding functionality to add button

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const btn = document.querySelector('[data-form-btn]');
 
 
-btn.addEventListener('click', function () {
+const createTask = (evento) => {
+    evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
-});
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = "";
+
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete()); 
+    const titleTask = document.createElement('span');
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+
+    const content = `
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+    task.appendChild(taskContent);
+    list.appendChild(task); 
+}
+
+btn.addEventListener('click', createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far")
+    i.classList.add("fa-check-square")
+    i.classList.add("icon")
+    
+    return i;
+}
