a8d66e711da61b93eccd60fa7df837e6ea81cb71
jesusv916 <jevamon916@hotmail.com>
2023-03-30 18:27:49 -0500

    Adding the trash icon and make it functional

--- script.js
+++ script.js
@@ -19,12 +19,10 @@ const createTask = (evento) => {
     titleTask.innerText = value;
     taskContent.appendChild(titleTask);
 
-    const content = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-
     task.appendChild(taskContent);
     list.appendChild(task); 
-}
+    task.appendChild(deleteIcon());
+};
 
 btn.addEventListener('click', createTask);
 
@@ -34,12 +32,26 @@ const checkComplete = () => {
     i.addEventListener("click", completeTask)
     
     return i;
-}
+};
 
 const completeTask = (event) => {
     const element = event.target;
     element.classList.toggle("fas");
     element.classList.toggle("completeIcon");
     element.classList.toggle("far");
-}
+};
+
+const deleteIcon = () => {
+    const i = document.createElement("i");
+    i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+    i.addEventListener("click", deleteTask)
+
+    return i
+};
+
+const deleteTask = (event) => {
+    const parent = event.target.parentElement;
+    parent.remove();
+};
+
 })()
